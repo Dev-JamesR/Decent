145111deffe7efaa1b7e2ef8431f5c7ac84ce720
FallenGravity <ethanvb55@gmail.com>
2019-12-03 17:08:56 +0200

    Add Slides to Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'slides',
+    loadChildren: () => import('./slides/slides.module').then( m => m.SlidesPageModule)
   }
 ];
 @NgModule({
